cfc3bf5c0191db175d106f7c26588e0fe0b5db1f
parsh <parshchakurkar@gmail.com>
2020-11-29 13:39:41 -0700

    New condition added to improve the chances of getting more 6

--- Dice_game.py
+++ Dice_game.py
@@ -1,4 +1,13 @@
 from random import randint
+
 # importing random module to provide random action
-Dice_number = randint(1, 6)
-print(Dice_number)
+
+print("Do you want to improve the chances of getting more 6 \n")
+selection = input(print("Enter Y for yes and N for No\n"))
+if selection == 'Y':  # Applying condition for getting more 6 on dice
+    num = randint(4, 6)
+    print(num)# it will give you number in between 4 to 6
+elif selection == 'N':
+    Dice_number = randint(1, 6)# it will give you number in between 1 to 6
+    print(Dice_number)
+print()
