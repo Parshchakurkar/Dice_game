0bf4e2365dadd25f98b62d0af39a583e1323d2f2
parsh <parshchakurkar@gmail.com>
2020-12-01 17:31:58 -0700

    the if statement condition updted and add to select in probability and normal

--- Dice_game.py
+++ Dice_game.py
@@ -1,25 +1,26 @@
 from random import randint
 import time
+
 # importing random module to provide random action
-#Time module to get some time to roll the dice
+# Time module to get some time to roll the dice
 
 selection = input(print("Do you want to improve the chances of getting more 6? \n(Y/N)"))
 
-if selection == 'Y' or 'y':
+if selection == 'Y' or selection == 'y':
     # Applying condition for getting more 6 on dice
     roll_dice = "Yes"
-    while roll_dice == "Yes" or roll_dice =='Y' or roll_dice =='y':
+    while roll_dice == "Yes" or roll_dice == 'Y' or roll_dice == 'y':
         print('getting number')
         time.sleep(1)
         num = randint(3, 6)
         print(num)
-    # it will give you number in between 4 to 6
+        # it will give you number in between 3 to 6
         roll_dice = input('Do you want to play again? (Y/N)')
 
 else:
-# normal Dice 1 to 6
+    # normal Dice 1 to 6
     roll_dice = "Yes"
-    while roll_dice == "Yes" or roll_dice =='Y' or roll_dice =='y':
+    while roll_dice == "Yes" or roll_dice == 'Y' or roll_dice == 'y':
         print('getting number')
         time.sleep(1)
         Dice_number = randint(1, 6)
