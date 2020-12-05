887284284d408a527a01327c6f39c2efa2214afa
parsh <parshchakurkar@gmail.com>
2020-12-05 11:46:54 -0700

    Function working for rolling the Dice

--- Dice_game.py
+++ Dice_game.py
@@ -1,12 +1,15 @@
 from random import randint
 import time
+
+
 # importing random module to provide random action
 # Time module to get some time to roll the dice
 
-def roll_over():
+
+def roll_over(x, y):
     time.sleep(1)
-    replay_num = randint(1, 6)
-    return replay_num
+    result = randint(x, y)
+    return result
 
 
 selection = input("Do you want to improve the chances of getting more 6? \n(Y/N)")
@@ -16,17 +19,19 @@ if selection == 'Y' or selection == 'y':
     roll_dice = "Yes"
     while roll_dice == "Yes" or roll_dice == 'Y' or roll_dice == 'y':
         print('getting number')
-        time.sleep(1)
-        num = randint(3, 6)
+        num = roll_over(3, 6)
+        #will give number within 3 to 6
         print(num)
         if num == 6:
+            # If you get 6 it will run again and give number from1 to 6
             replay = input('Play again press Enter')
-            time.sleep(1)
-            replay_num = randint(1, 6)
+            replay_num = roll_over(1, 6)
             print(replay_num)
             while replay_num == 6:
+                # If you get 6 it will run again and give number from1 to 6
                 replay = input('Play again press Enter')
-                roll_over()
+                replay_num = roll_over(1, 6)
+                print(replay_num)
         # it will give you number in between 3 to 6
         roll_dice = input('Do you want to play again? (Y/N)')
 
@@ -34,19 +39,19 @@ else:
     # normal Dice 1 to 6
     roll_dice = "Yes"
     while roll_dice == "Yes" or roll_dice == 'Y' or roll_dice == 'y':
-        print('getting number')
-        time.sleep(1)
-        Dice_number = randint(1, 6)
-        # it will give you number in between 1 to 6
-        print(Dice_number)
-        if Dice_number == 6:
+        num = roll_over(1, 6)
+        #will give number within 1 to 6
+        print(num)
+        if num == 6:
             replay = input('Play again press Enter')
-            time.sleep(1)
-            replay_num = randint(1, 6)
+            replay_num = roll_over(1, 6)
+            # If you get 6 it will run again and give number from1 to 6
             print(replay_num)
             while replay_num == 6:
                 replay = input('Play again press Enter')
-                roll_over()
+                replay_num = roll_over(1, 6)
+                # If you get 6 it will run again and give number from1 to 6
+                print(replay_num)
         roll_dice = input('Do you want to play again? (Y/N)')
 
 print()
